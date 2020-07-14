be637c40df659d29f8574b900b54af7f9d357a15
Liuba Grusha <liuba.ip@gmail.com>
2020-07-14 05:31:14 +0300

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/service/VetService.java
@@ -0,0 +1,13 @@
+package guru.springframework.service;
+
+import guru.springframework.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
